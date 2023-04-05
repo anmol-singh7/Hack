2c84eef6817fb17745d5d8be1245bdf3bcff32d9
code-Raghav <araghav2003@gmail.com>
2023-04-05 22:55:46 +0530

    .

---
+++ components/MedicalHistory.js
@@ -0,0 +1,7 @@
+export default function MedicalHistory() {
+  return (
+    <>
+      <main>Medical History</main>
+    </>
+  );
+}
--- pages/User.js
+++ pages/User.js
@@ -1,9 +1,12 @@
+import MedicalHistory from "@/components/MedicalHistory";
 import Navbar from "@/components/Navbar";
+import NearbyHospitals from "@/components/NearbyHospitals";
 
 export default function User() {
   return (
     <>
       <Navbar />
+      <MedicalHistory />
     </>
   );
 }
